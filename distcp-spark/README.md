5a1246638e5d08a815bf2bfae09f46063bf2dd44
listart <listart@qq.com>
2021-08-25 12:01:19 +0800

    0815 倒排索引题

---
+++ distcp-spark/README.md
@@ -0,0 +1,43 @@
+# Spark Core 编程作业
+
+作业内容和要求见 https://u.geekbang.org/lesson/162?article=408443
+
+
+
+## 排坑记录
+
+
+
+
+
+## 作业输出
+
+### 第一题 使用RDD API实现带词频的倒排索引
+
+源码见 [listart.InvertedIndexApp#main](src/main/scala/listart/InvertedIndexApp.scala)
+
+输入文件 `distcp-spark/input/data`
+
+```
+0. "it is what it is"
+1. "what is it"
+2. "it is a banana"
+```
+
+输出文件 `distcp-spark/output/output/part-00000`
+
+```
+"a":	{(2,1)}
+"banana":	{(2,1)}
+"is":	{(0,2), (1,1), (2,1)}
+"it":	{(0,2), (1,1), (2,1)}
+"what":	{(0,1), (1,1)}
+```
+
+
+
+## 参考资料
+
+1. [Inverted Index in Spark (scala)](https://www.slideshare.net/deanwampler/spark-the-next-top-compute-model-39976454/37-Inverted_Index_in_Spark_Scala) from slideshare.net
+2. [Tutorial: Create a Scala Maven application for Apache Spark in HDInsight using IntelliJ](https://docs.microsoft.com/en-us/azure/hdinsight/spark/apache-spark-create-standalone-application) from microsoft.com
+
---
+++ distcp-spark/src/main/scala/listart/InvertedIndexApp.scala
@@ -0,0 +1,34 @@
+package listart
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+/**
+ * Inverted index
+ * word => {(path,wordCount),...}
+ */
+object InvertedIndexApp {
+  def main(args: Array[String]): Unit = {
+    val dataFile = "distcp-spark/input/data"
+    val output = "distcp-spark/output/output"
+    val conf = new SparkConf().setMaster("local").setAppName("Inverted Index")
+    val sc = new SparkContext(conf)
+
+    val rdd = sc.textFile(dataFile)
+      .map(_.split(". ",2))
+      .flatMap(line => line(1)
+        .replaceAll("^\"|\"$", "")
+        .split(" ")
+        .map((line(0), _)))
+      .map((_, 1))
+      .reduceByKey(_+_)
+      .map({case ((p, w), n) => (w, (p, n))})
+      .groupByKey()
+      .sortByKey()
+      .map({case (w, seq) => "\"" + w + "\":\t" + seq.toList.sorted.mkString("{", ", ", "}")})
+
+    rdd.saveAsTextFile(output)
+
+    println(rdd.collect().mkString("Array(", ", ", ")"))
+  }
+}
